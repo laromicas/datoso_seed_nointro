b51479efdfe996d36e14cc1632ca35dba324ada6
laromicas <laromicas@hotmail.com>
2023-04-27 00:43:23 -0500

    Pumped version for deploy

--- src/datoso_seed_nointro/__init__.py
+++ src/datoso_seed_nointro/__init__.py
@@ -2,7 +2,12 @@
 __init__.py
 """
 __all__ = ["__version__", "__author__", "__description__"]
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 __author__ = "Lacides Miranda"
-__description__ = "Datoso plugin for seed Nointro"
+__description__ = "No-Intro is a collection of DATs aimed for video game preservation especialized in cartridge-based and digital systems."
 __preffix__ = "nointro"
+__requirements__ = {
+    'executables': [
+        'geckodriver',
+    ]
+}
